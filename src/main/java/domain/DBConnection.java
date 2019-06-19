d197d3c78f253fb971421c7b3cbfb24e673b8b7f
amdursu <amdursu@gmail.com>
2019-06-20 01:53:52 +0300

    May be workinggg

--- src/main/java/domain/DBConnection.java
+++ src/main/java/domain/DBConnection.java
@@ -24,8 +24,8 @@ public class DBConnection {
         //Context ctx = new InitialContext();
         //DataSource dts = (DataSource) ctx.lookup("jdbc/PetrolHead");
         PoolProperties p = new PoolProperties();
-          //p.setUrl(System.getenv("JDBC_DATABASE_URL"));
-          p.setUrl("jdbc:postgresql://localhost:5432/andrei");
+          p.setUrl(System.getenv("JDBC_DATABASE_URL"));
+          //p.setUrl("jdbc:postgresql://localhost:5432/andrei");
           p.setDriverClassName("org.postgresql.Driver");
           p.setUsername("andrei");
           p.setPassword("a");
